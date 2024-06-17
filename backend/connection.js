680971de6d88f320b297d6df5d554e819097bfdd
AjayPratapS1 <ajaypratapsingh8958a@gmail.com>
2024-06-18 00:10:07 +0530

    vercel deploy

--- backend/connection.js
+++ backend/connection.js
@@ -1,10 +1,13 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 var mysqlConnection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: "root",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_DBNAME,
+  waitForConnections:true,
+  connectionLimit:10,
+  queueLimit:0,
 });
 mysqlConnection.connect((error) => {
   if (error) {
