eb85be55443b2d340171969b41fdb8ea0dc083cf
AjayPratapS1 <ajaypratapsingh8958a@gmail.com>
2024-06-18 01:07:51 +0530

    update

--- frontend/src/components/DetailsForm.jsx
+++ frontend/src/components/DetailsForm.jsx
@@ -16,7 +16,7 @@ const DetailsForm = () => {
     // console.log("user Data", data);
     try {
       setLoading(true);
-      const res = await axios.post('https://formassignmet.vercel.app/api/v1/user', data);
+      const res = await axios.post('https://form-uofq.onrender.com/api/v1/user', data);
       toast("Data Save Successfully!");
       // console.log("result", res);
       setLoading(false);
